4ad9c31392b1bf9979e111a8089c8be27117764b
Yury2008 <yurygabriel2019@gmail.com>
2025-06-19 09:41:09 -0300

    alunosNotas.c

--- Aula 14/alunosNotas.c
+++ Aula 14/alunosNotas.c
@@ -5,12 +5,18 @@ int main()
   FILE *arquivo = fopen("notas.txt", "r");
   char aluno[30];
 
-  while (fgets(aluno, 30, arquivo) != NULL) 
+  if(arquivo != NULL)
   {
-    printf("%s", aluno);  
+    while (fgets(aluno, 30, arquivo) != NULL) 
+    {
+      printf("%s", aluno);  
+    }
+    fclose(arquivo);  
   }
 
-  fclose(arquivo);  
-  
+  else
+  {
+    printf("\nErro ao encontrar o arquivo\n);
+  }
   return 0;
 }
